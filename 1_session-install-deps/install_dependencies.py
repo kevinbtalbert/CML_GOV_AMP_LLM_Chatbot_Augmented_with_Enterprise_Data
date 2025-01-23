eb7f7ca22e3bf9f7a5ecd7597cff9b726f8d94db
Kevin Talbert <ktalbert@cloudera.com>
2025-01-23 17:32:30 -0500

    updated requirements

--- 1_session-install-deps/install_dependencies.py
+++ 1_session-install-deps/install_dependencies.py
@@ -1,7 +1,6 @@
 !pip install --upgrade pip
 !pip install --no-cache-dir --log 1_session-install-deps/pip-req.log -r 1_session-install-deps/requirements.txt
 
-!pip install chroma-hnswlib==0.7.3
-!pip install chromadb==0.5.0
-!pip install pysqlite3-binary==0.5.4
+# !pip install chromadb==0.5.0
+# !pip install pysqlite3-binary==0.5.4
 !pip install tokenizers==0.13.0
